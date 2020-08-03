298730baf145bc5e6254dbc848bf486683d26aea
paul <paul@nms.com.au>
2020-08-03 16:46:23 +1000

    list widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,24 +10,25 @@ class FmtApp extends StatelessWidget {
     return MaterialApp(
       title: "MyApplication",
       home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Row & Column'),
-        ),
-        body: new Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text('this'),
-            new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Text('is'),
-                new Text('a'),
-              ],
-            ),
-            new Text('row'),
-          ],
-        ),
-      ),
+          appBar: new AppBar(
+            title: new Text('Row & Column'),
+          ),
+          body: new ListView(
+            children: <Widget>[
+              new ListTile(
+                title: new Text('tile#1'),
+                trailing: new Icon(Icons.arrow_forward),
+              ),
+              new ListTile(
+                title: new Text('tile#2'),
+                trailing: new Icon(Icons.arrow_forward),
+              ),
+              new ListTile(
+                title: new Text('tile#3'),
+                trailing: new Icon(Icons.arrow_forward),
+              ),
+            ],
+          )),
     );
   }
 }
