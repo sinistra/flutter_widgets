256775a90c786fd88607cd9b85e7cdd7184f0631
paul <paul@nms.com.au>
2020-08-03 15:26:28 +1000

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    new Center(
-      child: new Text('first flutter app', textDirection: TextDirection.ltr,)
-    )
+    new FmtApp()
   );
 }
+
+class FmtApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Stateless Widget',
+      home : new Scaffold(
+        body: new Container(
+          color: Colors.green,
+          child: new Container(
+            color: Colors.amber,
+            margin: EdgeInsets.all(60.0),
+          ),
+        ),
+      )
+    );
+  }
+}
+
 
