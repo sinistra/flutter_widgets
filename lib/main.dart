1e26a1e63555297f7d5914c613e8858f87b005c0
paul <paul@nms.com.au>
2020-08-03 16:06:25 +1000

    container widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    new FmtApp()
-  );
+  runApp(new FmtApp());
 }
 
-class FmtApp extends StatefulWidget {
-  @override
-  _FmtAppState createState() => _FmtAppState();
-}
-
-class _FmtAppState extends State<FmtApp> {
-
-  String ttext = '';
-  @override
-  void initState() {
-    ttext = 'Click this button';
-    super.initState();
-  }
-
-  void changeButton() {
-    setState(() {
-      ttext = 'Button changed';
-    });
-  }
-
+class FmtApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Stateful widget',
+    return MaterialApp(
       home: new Scaffold(
-        body: new Center(
-          child: new RaisedButton(onPressed: () {changeButton();}, child: new Text(ttext, textDirection: TextDirection.ltr,),),
+        body: new Container(
+//          color: Colors.pink,
+          child: new Text('this is a container'),
+          height: 300.0,
+          width: 300.0,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(20.0),
+          decoration: new BoxDecoration(color: Colors.green),
+//          transform: new Matrix4.rotationZ(0.5),
+          foregroundDecoration: new BoxDecoration(
+            color: Colors.deepOrange,
+          ),
         ),
-      )
+      ),
     );
   }
 }
-
