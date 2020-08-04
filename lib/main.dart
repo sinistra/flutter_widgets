8beae760654582bd2794d24671ea1eb6b2fa04b7
paul <paul@nms.com.au>
2020-08-04 14:42:18 +1000

    snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'otherpage.dart';
 
 void main() {
   runApp(new MaterialApp(home: new Application()));
@@ -11,52 +10,24 @@ class Application extends StatefulWidget {
 }
 
 class _ApplicationState extends State<Application> {
+  final GlobalKey<ScaffoldState> _scaffoldkey = new GlobalKey<ScaffoldState>();
 
+  void _method1() {
+    _scaffoldkey.currentState
+        .showSnackBar(new SnackBar(content: new Text('Show the Snackbar')));
+  }
 
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text('Drawer'),),
-      drawer: new Drawer(
-          child: new ListView(
-            children: <Widget>[
-              new UserAccountsDrawerHeader(
-                accountName: new Text('Paul Taylor'),
-                accountEmail: new Text('dummy@email.com'),
-                currentAccountPicture: new CircleAvatar(
-                  backgroundColor: Colors.black26,
-                  child: new Text('S'),
-                ),
-                decoration: new BoxDecoration(color: Colors.orange),
-              ),
-              new ListTile(
-                title: new Text('Page 1'),
-                trailing: new Icon(Icons.arrow_forward),
-                onTap: ()=> Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context)=>new op('page1'))),
-              ),
-              new ListTile(
-                title: new Text('Page 2'),
-                trailing: new Icon(Icons.arrow_forward),
-                onTap: ()=> Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context)=>new op('page2'))),
-
-              ),
-              new ListTile(
-                title: new Text('Close'),
-                trailing: new Icon(Icons.close),
-                onTap: (){Navigator.pop(context);},
-              ),
-            ],
-          )
+      key: _scaffoldkey,
+      body: new Center(
+        child: new RaisedButton(
+          onPressed: () {
+            _method1();
+          },
+          child: new Text('Activate Snackbar'),
+        ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
