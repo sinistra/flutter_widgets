b9f9cc984d5393972bffc3d8f269f298f5a81adc
paul <paul@nms.com.au>
2020-08-04 11:51:12 +1000

    appBar widget with actions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(new MaterialApp(home: new Application()));
 }
@@ -11,27 +10,44 @@ class Application extends StatefulWidget {
 }
 
 class _ApplicationState extends State<Application> {
+  String msg = '';
 
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Appbar'),
-        backgroundColor: Colors.green,
-        elevation: 30.0,
-        centerTitle: true,
-//        titleSpacing: 70.0,
-//        toolbarOpacity: 0.5,
+        leading: new Icon(Icons.menu),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.arrow_forward),
+            onPressed: () {
+              setState(() {
+                msg = 'you pressed Arrow Button';
+              });
+              print('you pressed Arrow Button');
+            },
+          ),
+          new IconButton(
+              icon: new Icon(Icons.add),
+              onPressed: () {
+                setState(() {
+                  msg = 'you pressed Add Button';
+                });
+                print('you pressed Add Button');
+              }),
+          new IconButton(
+              icon: new Icon(Icons.close),
+              onPressed: () {
+                setState(() {
+                  msg = 'you pressed Close Button';
+                });
+                print('you pressed Close Button');
+              }),
+        ],
+      ),
+      body: new Center(
+        child: new Text(msg),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
